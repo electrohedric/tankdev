4bb9753d6b892a0ec2fd3cef018a6c3a467dc42e
Nick Stames <nstames0@gmail.com>
2018-03-23 16:57:12 -0400

    fixed
    got rid of stuff that made it break

--- Stains/src/staindev/Main.java
+++ Stains/src/staindev/Main.java
@@ -3,13 +3,7 @@ package staindev;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		
 		System.out.println("Hello world!");
-
-=======
-		System.out.println("Nick Sux");
->>>>>>> origin/master
 	}
 
 }
