77ed472da0ab876ae693fae043f40a15f4b2fbf4
Nick Stames <nstames0@gmail.com>
2018-04-01 16:40:19 -0400

    made it work
    there is now a red triangle
    test to see if it works on your computer, if not we have a problem

---
+++ Stains/src/constants/Sizeof.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class Sizeof {
+	public static final int FLOAT = 4;
+	public static final int INT = 4;
+	public static final int CHAR = 2;
+	public static final int LONG = 8;
+}
--- Stains/src/staindev/Main.java
+++ Stains/src/staindev/Main.java
@@ -1,36 +1,30 @@
 package staindev;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.IntBuffer;
-import java.util.HashMap;
-import java.util.Map;
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
 
-import org.lwjgl.Version;
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.system.MemoryStack;
-import org.newdawn.slick.Color;
-import org.newdawn.slick.opengl.Texture;
-import org.newdawn.slick.opengl.TextureLoader;
-import org.newdawn.slick.util.ResourceLoader;
+import constants.Sizeof;
+
+import java.nio.*;
 
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
-
 public class Main {
-	
+
 	// The window handle
 	private long window;
-	public Map<String, Texture> textures = new HashMap<>();
-	public static final int WIDTH = 800, HEIGHT = 600;
 
 	public void run() {
+		System.out.println("LWJGL version " + Version.getVersion());
+
 		init();
 		loop();
 
@@ -44,8 +38,12 @@ public class Main {
 	}
 
 	private void init() {
+		// Setup an error callback. The default implementation
+		// will print the error message in System.err.
 		GLFWErrorCallback.createPrint(System.err).set();
-		if (!glfwInit())
+
+		// Initialize GLFW. Most GLFW functions will not work before doing this.
+		if ( !glfwInit() )
 			throw new IllegalStateException("Unable to initialize GLFW");
 
 		// Configure GLFW
@@ -54,18 +52,18 @@ public class Main {
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
 		// Create the window
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Stain Game", NULL, NULL);
-		if(window == NULL)
+		window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+		if ( window == NULL )
 			throw new RuntimeException("Failed to create the GLFW window");
 
 		// Setup a key callback. It will be called every time a key is pressed, repeated or released.
 		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-			if(key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+			if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
 				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
 		});
 
 		// Get the thread stack and push a new frame
-		try(MemoryStack stack = stackPush()) {
+		try ( MemoryStack stack = stackPush() ) {
 			IntBuffer pWidth = stack.mallocInt(1); // int*
 			IntBuffer pHeight = stack.mallocInt(1); // int*
 
@@ -90,64 +88,100 @@ public class Main {
 
 		// Make the window visible
 		glfwShowWindow(window);
-		
-		System.out.println("Loading TEXTURES");
-		loadTextures();
 	}
 
 	private void loop() {
-		// This line is critical for LWJGL's interoperation with GLFW
+		// This line is critical for LWJGL's interoperation with GLFW's
+		// OpenGL context, or any context that is managed externally.
+		// LWJGL detects the context that is current in the current thread,
+		// creates the GLCapabilities instance and makes the OpenGL
+		// bindings available for use.
 		GL.createCapabilities();
+		System.out.println("OpenGL version " + glGetString(GL_VERSION));
+
+		// Set the clear color
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		
-		// render
-		while(!glfwWindowShouldClose(window)) {
+		float positions[] = {
+			-0.5f, -0.5f,
+			 0.0f,  0.5f,
+			 0.5f, -0.5f
+		};
+		
+		int buffer = glGenBuffers();
+		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		glBufferData(GL_ARRAY_BUFFER, positions, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, false, Sizeof.FLOAT * 2, 0);
+		
+		String vertex = 
+				"#version 330 core\n" +
+				"layout(location = 0) in vec4 pos;\n" +
+				"void main() {\n" +
+				"  gl_Position = pos;\n" +
+				"}";
+		
+		String fragment = 
+				"#version 330 core\n" +
+				"layout(location = 0) out vec4 color;\n" +
+				"void main() {\n" +
+				"  color = vec4(1.0, 0.0, 0.0, 1.0);\n" +
+				"}";
+		
+		int program = createShader(vertex, fragment);
+		glUseProgram(program);
+		
+		// Run the rendering loop until the user has attempted to close
+		// the window or has pressed the ESCAPE key.
+		while ( !glfwWindowShouldClose(window) ) {
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-			glfwSwapBuffers(window); // swap the color buffers
-			glfwPollEvents();
 			
-			drawQuad(textures.get("staintest"), 1, 1, 64, 64);
+			// RENDER
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+			
+			
+			glfwSwapBuffers(window); // swap the color buffers
+			
+			// Poll for window events. The key callback above will only be
+			// invoked during this call.
+			glfwPollEvents();
 		}
 	}
 	
-	public void loadTextures() {
-		loadTexture("water", "staintest");
-	}
-	
-	public void loadTexture(String imageName, String key) {
-		InputStream in = ResourceLoader.getResourceAsStream("res/" + imageName + ".png");
-		Texture texture = null;
-		try {
-			texture = TextureLoader.getTexture("PNG", in);
-		} catch (IOException e) {
-			e.printStackTrace();
+	private int compileShader(String source, int type) {
+		int shader = glCreateShader(type);
+		glShaderSource(shader, source);
+		glCompileShader(shader);
+		int[] result = new int[1];
+		glGetShaderiv(shader, GL_COMPILE_STATUS, result);
+		if(result[0] == GL_FALSE) { // bad compile
+			String log = glGetShaderInfoLog(shader);
+			System.err.println("Failed to compile " + (type == GL_VERTEX_SHADER ? "VERTEX" : "FRAGMENT") + " shader.\n");
+			System.err.flush();
+			System.out.println(log + "\n");
 		}
-		textures.put(key, texture);
+		return shader;
 	}
 	
-	public void loadTexture(String imageName) {
-		loadTexture(imageName, imageName);
-	}
-	
-	public void drawQuad(Texture texture, int x, int y, int width, int height) {
-		int w = texture.getTextureWidth();
-		int h = texture.getTextureHeight();
-		texture.bind();
-		glTranslatef(x, y, 0);
-		glBegin(GL_QUADS);
-			glTexCoord2f(0, 0);
-			glVertex2f(0, 0);
-			glTexCoord2f(w, 0);
-			glVertex2f(width, 0);
-			glTexCoord2f(w, h);
-			glVertex2f(width, height);
-			glTexCoord2f(0, h);
-			glVertex2f(0, height);
-		glEnd();
-		glLoadIdentity();
+	private int createShader(String vertex, String fragment) {
+		int program = glCreateProgram();
+		
+		int vs = compileShader(vertex, GL_VERTEX_SHADER);
+		int fs = compileShader(fragment, GL_FRAGMENT_SHADER);
+		
+		glAttachShader(program, vs);
+		glAttachShader(program, fs);
+		glLinkProgram(program);
+		glValidateProgram(program);
+		
+		glDeleteShader(vs);
+		glDeleteShader(fs);
+		
+		return program;
 	}
 
 	public static void main(String[] args) {
 		new Main().run();
 	}
+
 }
